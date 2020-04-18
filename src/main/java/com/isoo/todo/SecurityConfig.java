8c11f7ad0be409ff650dc387b874bbaaf7010150
root <isoo.isurunath@gmail.com>
2020-04-18 18:19:58 +1000

    Authuntication using DB

--- src/main/java/com/isoo/todo/SecurityConfig.java
+++ src/main/java/com/isoo/todo/SecurityConfig.java
@@ -1,14 +1,25 @@
 package com.isoo.todo;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+    private DataSource datasource;
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().
@@ -19,6 +30,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("isurunath").password("{noop}root").roles("USER");
+		
+		//hard coded username and password
+		//auth.inMemoryAuthentication().withUser("isurunath").password("{noop}root").roles("USER");
+		
+		//get username and password from DB
+		auth.jdbcAuthentication().dataSource(datasource)
+        .authoritiesByUsernameQuery("select username, role from user where username=?")
+        .usersByUsernameQuery("select username, password, 1 as enabled  from user where username=?");
 	}
+	
+	@Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
--- src/main/java/com/isoo/todo/TodoApplication.java
+++ src/main/java/com/isoo/todo/TodoApplication.java
@@ -1,7 +1,12 @@
 package com.isoo.todo;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TodoApplication {
@@ -9,5 +14,11 @@ public class TodoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
 	}
+	
+	@Bean(value = "datasource")
+    @ConfigurationProperties("spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
 
 }
